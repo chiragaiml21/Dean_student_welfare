aab1e16765bbfde67c1acc8a2b2040c6e9fb2b79
chiragaiml21 <bharathakur.palwal@gmail.com>
2023-04-08 14:48:05 +0530

    update 3 commit

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ function toggleTab(tabId) {
 var tab = document.getElementById(tabId);
 var content = document.getElementById("content" + tabId.slice(3));
 
+// ---------tab closing problem---------
+
+// tab.forEach(function (t) {
+//     if (t.id !== tabId) {
+//       t.classList.remove("active");
+//     }
+//   });
+//   content.forEach(function (c) {
+//     if (c.id !== "content" + tabId.slice(3)) {
+//       c.classList.remove("active");
+//     }
+//   });
+
+
 tab.classList.toggle("active");
 content.classList.toggle("active");
 }
