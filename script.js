646296d81482bd789810095132cceb5ad622b58e
chiragaiml21 <bharathakur.palwal@gmail.com>
2023-04-08 08:41:41 +0530

    new commit

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let btn = document.querySelector(".menu_btn");
 let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector("#search");
+
 
 btn.onclick= function(){
     sidebar.classList.toggle("active");
 }
-searchBtn.onclick= function(){
-    sidebar.classList.toggle("active");
-}
+
 
 
 function toggleTab(tabId) {
